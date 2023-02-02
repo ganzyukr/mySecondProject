738b9e79c697020749b08938d3eed31f099b3b08
tatiana <ganzyukr@gmail.com>
2023-02-02 17:33:35 +0200

    Init commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+
+    static void showMessage(String message){
+        System.out.println(message);
+    }
+    public static void main(String[] args) {
+        showMessage("Hi");
+    }
+}
