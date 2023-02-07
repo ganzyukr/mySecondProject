b84ad934f5351ce3d86829dabf06415e62860528
tatiana <ganzyukr@gmail.com>
2023-02-07 14:36:10 +0200

    HW2_num1+num2+num3

--- src/hw2/HW2.java
+++ src/hw2/HW2.java
@@ -1,27 +1,31 @@
 package hw2;
 
-import jdk.jfr.internal.tool.Main;
-
 import java.util.Scanner;
 
 public class HW2 {
-//    public static void main(String[] args) {
-//
-//        int a = 1;
-//        int b = 2;
-//        int c = 3;
-//
-//        int sum = a + b + c;
-//
-//        System.out.println(sum);
-//
-//    }
+
+    public static void main(String[] args) {
+
+        int a = 123;
+        System.out.println("Сума цифр числа " + a + " дорівнює " + ((a % 10) + ((a / 10) % 10) + ((a / 100) % 10)));
 
 
-        public static void main(String[] args){
-            int a = 123;
-            System.out.println("Сумма цифр числа " +a +" равна "+((a%10)+((a/10)%10)+((a/100)%10)));
+        String Str = new String("Hello World");
+        System.out.print("Довжина двох слів \"Hello World\" - ");
+        System.out.println(Str.length());
+
+
+        Scanner in = new Scanner(System.in);
+        System.out.print("Введіть число: ");
+        int input = in.nextInt();
+
+        if (input > 0) {
+            System.out.println("true - " + "Ваше число більше нуля");
+        } else if (input < 0) {
+            System.out.println("false - " + "Ваше число меньше нуля");
+        } else {
+            System.out.println("Число равно нулю ");
         }
+
     }
-
-
+}
