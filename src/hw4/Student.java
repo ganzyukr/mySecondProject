3e86f216f62c39211aa51ff45e8e3095cacabd39
tatiana <ganzyukr@gmail.com>
2023-02-12 12:06:51 +0200

    HW4_num1_final

--- src/hw4/Student.java
+++ src/hw4/Student.java
@@ -9,19 +9,9 @@ public class Student {
     double scoreMathematics;
     double scoreEconomics;
     double scoreForeignLanguage;
-    static double average;
-
-
-    private double rating;
-    static double avgRating;
-
-
-
 
 
     public Student(char studentIdNumber, String name, String surname, int course, double scoreMathematics, double scoreEconomics, double scoreForeignLanguage) {
-
-
 
         this.studentIdNumber = studentIdNumber;
         this.name = name;
@@ -30,10 +20,69 @@ public class Student {
         this.scoreMathematics = scoreMathematics;
         this.scoreEconomics = scoreEconomics;
         this.scoreForeignLanguage = scoreForeignLanguage;
+
     }
-
-
 }
+// Дуже багато часу витратила в пошуках правильної реалізації, намагалось щось придумати сама...
+// В голові тепер повна каша((
+// Нижче приклади які я намагалась підкорегувати під завдання
+
+
+//    public char getStudentIdNumber() {
+//        return studentIdNumber;
+//    }
+//
+//    public void setStudentIdNumber(char studentIdNumber) {
+//        this.studentIdNumber = studentIdNumber;
+//    }
+//
+//    public String getName() {
+//        return name;
+//    }
+//
+//    public void setName(String name) {
+//        this.name = name;
+//    }
+//
+//    public double getScoreMathematics() {
+//        return scoreMathematics;
+//    }
+//
+//    public void setScoreMathematics(double scoreMathematics) {
+//        this.scoreMathematics = scoreMathematics;
+//    }
+//
+//    public double getScoreEconomics() {
+//        return scoreEconomics;
+//    }
+//
+//    public void setScoreEconomics(double scoreEconomics) {
+//        this.scoreEconomics = scoreEconomics;
+//    }
+//
+//    public double getScoreForeignLanguage() {
+//        return scoreForeignLanguage;
+//    }
+//
+//    public void setScoreForeignLanguage(double scoreForeignLanguage) {
+//        this.scoreForeignLanguage = scoreForeignLanguage;
+//    }
+//
+//    public double getAverage() {
+//        return average;
+//    }
+//
+//    public void setAverage(double average) {
+//        this.average = average;
+//    }
+//
+//    @Override
+//    public String toString() {
+//        return String.format(Locale.ENGLISH,"Школа № " + name + " математика - " + "%.1f" + " русский язык - " + "%.1f" + " инфрматика - " + "%.1f" + " общий средний балл - " + "%.1f", getscoreMathematics(), getscoreEconomics(),getscoreForeignLanguage());
+//    }
+//}
+
+
 //    public static void main(String[] args) {
 //        Student student1 = new Student();
 //        student1.scoreMathematics = 4.4;
--- src/hw4/StudentTest.java
+++ src/hw4/StudentTest.java
@@ -1,23 +1,58 @@
 package hw4;
 
 public class StudentTest {
+    public static void main(String[] args) {
 
+        Student student1 = new Student('1', "Ann", "Kildina", 3, 9.0, 8.0, 10.0);
+        Student student2 = new Student('2', "Alex", "Titov", 5, 8.0, 6.0, 7.0);
+        Student student3 = new Student('3', "Kate", "Homenko", 4, 10.3, 9.0, 10.7);
 
+        System.out.print("Значення середнього арифметичного student1 дорівнює: ");
+        System.out.println((student1.scoreMathematics + student1.scoreEconomics + student1.scoreForeignLanguage) / 3);
+        System.out.print("Значення середнього арифметичного student2 дорівнює: ");
+        System.out.println((student2.scoreMathematics + student2.scoreEconomics + student2.scoreForeignLanguage) / 3);
+        System.out.print("Значення середнього арифметичного student3 дорівнює: ");
+        System.out.println((student3.scoreMathematics + student3.scoreEconomics + student3.scoreForeignLanguage) / 3);
 
-
-            public static void main(String[] args) {
-        Student student1 = new Student('1', "Ann", "Kildina", 3, 9.6, 8.6, 10.2);
-        Student student2 = new Student('2', "Alex", "Titov", 5, 9.9, 8.3, 10.3);
-        Student student3 = new Student('3', "Kate", "Homenko", 4, 9.7, 8.7, 10.8);
-
-
-        System.out.println(student1.average);
-        System.out.println(student2.average);
-        System.out.println(student3.average);
-
-        System.out.println(student1);
     }
 }
+
+//        double[] student1 = {9.0, 8.0, 10.0};
+//        double result1 = 0;
+//        for (double m : student1) {
+//            result1 += m;
+//        }
+//
+//        double[] student2 = {9.7, 8.3, 10.2};
+//        double result2 = 0;
+//        for (double m : student2) {
+//            result2 += m;
+//        }
+//
+//        double[] student3 = {8.3, 9.0, 9.7};
+//        double result3 = 0;
+//        for (double m : student3) {
+//            result3 += m;
+//        }
+//        System.out.println("Значение среднего арифметического student1 равно: " + result1 / student1.length);
+//        System.out.println("Значение среднего арифметического student2 равно: " + result2 / student2.length);
+//        System.out.println("Значение среднего арифметического student3 равно: " + result3 / student3.length);
+
+
+//        System.out.println(student1.average);
+//        System.out.println(student2.average);
+//        System.out.println(student3.average);
+
+//    public static void main(String[] args) {
+//        double[] nums = {3.3, 4.4, 5.5};
+//        double result = 0;
+//
+//        for (double d : nums) {
+//            result += d;
+//        }
+//        System.out.println("dfdfd " + result / nums.length);
+//    }
+
 
 //        Scanner in = new Scanner(System.in);
 //        System.out.print("Введите размерность массива: ");
