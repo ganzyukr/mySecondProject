f7b2a18aafc7ee479c1c5fb08168f276519cb1e0
tatiana <ganzyukr@gmail.com>
2023-02-13 18:25:04 +0200

    HW5_num1+num2_OOP

--- src/hw5/num2/Animal.java
+++ src/hw5/num2/Animal.java
@@ -2,26 +2,39 @@ package hw5.num2;
 
 public class Animal {
 
-
     int eyes;
     String view;
 
+    public Animal(int eyes) {
+        this.eyes = eyes;
+    }
 
+    public Animal() {
+    }
 
-    //    void view(){
-//        System.out.println("I am animal");
-//    }
-    void eat(){
+    public String getView() {
+        return view;
+    }
+
+    public void setView(String view) {
+        this.view = view;
+    }
+
+    void eat() {
+
         System.out.println("Animal eats");
     }
-    void drink(){
+
+    void drink() {
+
         System.out.println("Animal drinks");
     }
 
     public static void main(String[] args) {
         Animal animal = new Animal();
+        animal.setView("I am animal");
+        System.out.println("Who are you? - " + animal.getView());
         animal.eat();
         animal.drink();
-        animal.view = "I am animal";
     }
 }
--- src/hw5/num2/Test.java
+++ src/hw5/num2/Test.java
@@ -1,7 +1,12 @@
 package hw5.num2;
 
 public class Test {
+
     public static void main(String[] args) {
 
+        Dog dog = new Dog();
+        System.out.println("Amount of paws of an object of the class Dog: " + dog.paw);
+        Cat cat = new Cat();
+        cat.sleep();
     }
 }
