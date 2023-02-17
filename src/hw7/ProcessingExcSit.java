0d665a4ad170411ac5c3e59da0c3d653e1d7195e
tatiana <ganzyukr@gmail.com>
2023-02-17 20:04:38 +0200

    HW7_num1_PhoneRegEex + num2_ProcessingExcSit

--- src/hw7/ProcessingExcSit.java
+++ src/hw7/ProcessingExcSit.java
@@ -1,4 +1,30 @@
 package hw7;
 
+import java.util.Arrays;
+
 public class ProcessingExcSit {
+
+    public static class ArrayDividing {
+        public void dividerArr(int[] arr, int divider) {
+            int[] output = new int[arr.length];
+            try {
+                for (int i = 0; i < arr.length; i++) {
+                    output[i] = arr[i] / divider;
+                }
+                System.out.println(Arrays.toString(output));
+            } catch (ArithmeticException e) {
+                System.out.print("Message: " + e);
+            }
+        }
+    }
+
+    public static void main(String args[]) {
+        int[] array = {4, 9, 6, 8};
+        int dividerNum = 2;
+        ArrayDividing arrayDividing = new ArrayDividing();
+        arrayDividing.dividerArr(array, dividerNum);
+    }
 }
+
+
+
